d14d21db6e058a8fe219456236a5eebb3555015b
DutraGames <gabrieldutraamarante@gmail.com>
2024-06-26 10:33:25 -0300

    feat: add new screen in Loading

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,15 +1,24 @@
 import { colors } from "@/styles/colors";
-import { ActivityIndicator } from "react-native";
-
+import { ActivityIndicator, Image, View } from "react-native";
 export function Loading() {
   return (
-    <ActivityIndicator
+    <View
       style={{
         flex: 1,
         backgroundColor: colors.red[500],
         justifyContent: "center",
         alignItems: "center",
       }}
-    />
+    >
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Image
+          source={require("../imgs/logo.png")}
+          style={{ width: 300, height: 100, objectFit: "contain" }}
+        />
+      </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color={colors.white} />
+      </View>
+    </View>
   );
 }
