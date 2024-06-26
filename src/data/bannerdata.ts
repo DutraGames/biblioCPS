2b753a7d05584d6ebc03e7f4e76ce5a5825780a7
DutraGames <gabrieldutraamarante@gmail.com>
2024-06-26 10:18:36 -0300

    fix: datbanner

--- src/data/bannerdata.ts
+++ src/data/bannerdata.ts
@@ -1,9 +1,14 @@
-import { faker } from "@faker-js/faker";
-
-export const bannerData = Array.from({ length: 5 }).map(() => {
-  return {
-    id: faker.number.int({ min: 1, max: 100 }),
-
-    image: faker.image.urlLoremFlickr({ category: "book" }),
-  };
-});
+export const bannerData = [
+  {
+    id: 1,
+    image: "https://m.media-amazon.com/images/I/71dH97FwGbL._SY385_.jpg",
+  },
+  {
+    id: 2,
+    image: "https://m.media-amazon.com/images/I/616V-DRk8aL._SY466_.jpg",
+  },
+  {
+    id: 3,
+    image: "https://m.media-amazon.com/images/I/71Vkg7GfPFL._SY425_.jpg",
+  },
+];
